997a6f5678d354f47d22dd90e8f64e2ffb1fc6dc
Rishabh Baid <53312269+dhulmul@users.noreply.github.com>
2019-08-03 19:38:50 +0200

    commands for installing docker

--- setup.sh
+++ setup.sh
@@ -19,3 +19,15 @@ sudo apt install zsh
 #Install ohmyzsh specific packages
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -L git.io/antigen > antigen.zsh
+#Setup docker
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   bionic stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
