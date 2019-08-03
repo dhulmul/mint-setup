1ed1a7a6df6ab6c66bb40bc26b75b8eb219a37ca
Rishabh Baid <53312269+dhulmul@users.noreply.github.com>
2019-08-03 16:10:16 +0200

    Commands for installing wifi drivers

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #Install git and other essential packages
 sudo apt-get install linux-headers-generic build-essential git
+#Install wifi driver
+git clone https://github.com/smlinux/rtl8723de.git -b 4.11-up
+dkms add ./rtl8723de
+dkms install rtl8723de/5.1.1.8_21285.20171026_COEX20170111-1414
+depmod -a
+reboot
 # Install snap 
 sudo apt install snapd
 # Instal VS Code
