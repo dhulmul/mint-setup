518ca4fd3acb05be06c709abedf651cbe680409f
Rishabh Baid <53312269+dhulmul@users.noreply.github.com>
2019-08-03 17:10:27 +0200

    dconf editor installation

--- setup.sh
+++ setup.sh
@@ -2,14 +2,15 @@
 sudo apt-get install linux-headers-generic build-essential git
 #Install wifi driver
 git clone https://github.com/smlinux/rtl8723de.git -b 4.11-up
-dkms add ./rtl8723de
-dkms install rtl8723de/5.1.1.8_21285.20171026_COEX20170111-1414
-depmod -a
-reboot
+sudo dkms add ./rtl8723de
+sudo dkms install rtl8723de/5.1.1.8_21285.20171026_COEX20170111-1414
+sudo depmod -a
+#reboot
 # Install snap 
 sudo apt install snapd
 # Instal VS Code
 sudo snap install --classic code
 # Instal Terminology
 sudo apt-get install terminology
-
+# Install dconf editor
+sudo apt-get install dconf-editor
