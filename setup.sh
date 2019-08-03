76093a781c6d68f6de6a061d0b7be5d0bf21dd44
Rishabh Baid <53312269+dhulmul@users.noreply.github.com>
2019-08-03 16:05:02 +0200

    Creating setup file

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#Install git and other essential packages
+sudo apt-get install linux-headers-generic build-essential git
+# Install snap 
+sudo apt install snapd
+# Instal VS Code
+sudo snap install --classic code
+# Instal Terminology
+sudo apt-get install terminology
+
