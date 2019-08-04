849426f2db26fcfd5e2e82de5fa28cb9dda96b06
Rishabh Baid <53312269+dhulmul@users.noreply.github.com>
2019-08-04 11:11:09 +0200

    Mongodb and pipenv

--- setup.sh
+++ setup.sh
@@ -33,5 +33,12 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 #Generate ssh key
 ssh-keygen -t rsa -C "rishabhbaid1@gmail.com"
-#Install pip
+#Install pip and pipenv
 sudo apt install python3-pip
+pip install --user pipenv
+#Install Mongo
+wget -qO - https://www.mongodb.org/static/pgp/server-4.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
